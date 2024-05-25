c140686704274cf31f4c2e2d01bc0ea6c12eee91
Keith Lea <keithl@gmail.com>
2024-05-25 14:37:35 -0400

    Generate checksum manifest. Also print more useful progress along the way

--- backup_dirs.py
+++ backup_dirs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from dataclasses import dataclass
 import hashlib
 import logging
 from pathlib import Path
@@ -10,6 +11,7 @@ import psutil
 import argparse
 import sys
 import humanize
+import csv
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
@@ -27,6 +29,12 @@ def sha256sum(filename):
 ROOT_S3_CAS = Path("cas")
 
 
+@dataclass
+class FileToProcess:
+    path: Path
+    size: int
+
+
 def main():
     parser = argparse.ArgumentParser(
         prog="ProgramName",
@@ -36,23 +44,67 @@ def main():
     parser.add_argument("bucket")
     parser.add_argument("dirs", nargs="+")
     parser.add_argument("--battery", action="store_true")
+    parser.add_argument("--output-csv")
 
     args = parser.parse_args()
     dirs = [Path(dir) for dir in args.dirs]
 
     if not args.battery and not psutil.sensors_battery().power_plugged:
-        print("Not doing anything; we're on battery!")
+        logging.warn("Not doing anything; we're on battery!")
         return 1
 
     s3_client = boto3.client("s3")
 
-    for dir in dirs:
-        for f in dir.glob("**/*"):
-            if f.is_dir():
-                continue
+    logging.info("Scanning directories...")
+    files_to_process = sorted(
+        (
+            FileToProcess(path=f.absolute(), size=f.stat().st_size)
+            for dir in dirs
+            for f in dir.glob("**/*")
+            if not (f.is_dir() or f.is_symlink())
+        ),
+        key=lambda f: str(f.path),
+    )
+
+    total_bytes = sum(f.size for f in files_to_process)
+
+    logging.info(
+        f"Will process {len(files_to_process):,} files, a total of {humanize.naturalsize(total_bytes)}"
+    )
+
+    output_csv_path = Path(args.output_csv)
+    logging.info(f"Writing to {output_csv_path.absolute()}")
+
+    with open(output_csv_path, "w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=("path", "sha256"))
+        writer.writeheader()
+
+        # for printing progress
+        percent_int = 0
+        processed_bytes = 0
+
+        for index, file_to_process in enumerate(files_to_process):
+            current_percent_int = int(index * 100.0 / len(files_to_process))
+            if percent_int != current_percent_int:
+                percent_int = current_percent_int
+                logging.info(
+                    f"Processed {index:,} of {len(files_to_process):,} files ({current_percent_int}%)"
+                )
+                logging.info(
+                    f"Processed {humanize.naturalsize(processed_bytes)} of {humanize.naturalsize(total_bytes)} ({round(processed_bytes * 100.0 / total_bytes)}%)"
+                )
+
+            processed_bytes += file_to_process.size
+
             try:
+                f = file_to_process.path
                 csum = sha256sum(f)
-                size_on_disk = f.stat().st_size
+
+                # store in manifest
+                writer.writerow({"path": f.absolute(), "sha256": csum})
+                csvfile.flush()
+
+                size_on_disk = file_to_process.size
                 path_in_s3 = str(ROOT_S3_CAS / csum[0] / csum[1] / csum)
                 try:
                     obj = s3_client.get_object(Bucket=args.bucket, Key=path_in_s3)
@@ -69,10 +121,10 @@ def main():
                 )
 
                 response = s3_client.upload_file(f.absolute(), args.bucket, path_in_s3)
-                print("success!")
+                logging.info("success!")
             except Exception as e:
-                print(f"error uploading {f.absolute()}")
-                print(e)
+                logging.warn(f"error uploading {f.absolute()}")
+                logging.warn(e)
 
     return 0
 
